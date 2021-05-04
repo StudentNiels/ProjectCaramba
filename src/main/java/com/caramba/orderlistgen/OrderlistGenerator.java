b98688fc83a704ada2b9f331ae80125b8839e9a8
Bram <X>
2021-05-04 14:13:17 +0200

    Configured gradle to build jar file using OrderlistGenerator as main.

---
+++ src/main/java/com/caramba/orderlistgen/OrderlistGenerator.java
@@ -0,0 +1,7 @@
+package com.caramba.orderlistgen;
+
+public class OrderlistGenerator {
+    public static void main(String[] args){
+        System.out.println("Orderlist generator started");
+    }
+}
