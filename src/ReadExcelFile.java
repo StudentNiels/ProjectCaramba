4671bd453dab0a3c1352d1ce6e72fef7a2c4de0a
Gharyan <simon_98@live.nl>
2021-05-07 13:07:03 +0200

    Apache poi werkend gekregen
    Map layout aangepast en nu werkt het.

--- src/ReadExcelFile.java
+++ src/ReadExcelFile.java
@@ -13,7 +13,7 @@ public class ReadExcelFile
 public static void main(String args[]) throws IOException
 {
     //obtaining input bytes from a file
-    try (InputStream inp = new FileInputStream("C:\\Users\\Simon the Aryan\\Downloads\\student.xls"))
+    try (InputStream inp = new FileInputStream("C:\Users\Simon the Aryan\Downloads\student.xlsx"))
     {
         //InputStream inp = new FileInputStream("student.xlsx");
         Workbook wb = WorkbookFactory.create(inp);
@@ -26,7 +26,7 @@ public static void main(String args[]) throws IOException
         cell.setCellValue("a test");
 
         // Write the output to a file
-        try (OutputStream fileOut = new FileOutputStream("C:\\Users\\Simon the Aryan\\Downloads\\student.xls"))
+        try (OutputStream fileOut = new FileOutputStream('C:\Users\Simon the Aryan\Downloads\student.xlsx'))
         {
             wb.write(fileOut);
         }
