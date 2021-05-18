3287511d1168d0f47463ba6762f1f3d206e6c810
Bram <X>
2021-05-18 10:27:02 +0200

    add VendorList unit test

--- src/test/java/com/caramba/ordertool/VendorListTest.java
+++ src/test/java/com/caramba/ordertool/VendorListTest.java
@@ -1,19 +1,43 @@
 package com.caramba.ordertool;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VendorListTest {
-    VendorList vl = new VendorList();
 
-    //Assure index out of bounds exception is getting caught in the function
-    @Test
-    void getVendor() {
-        String s = "test";
-        assertNull(vl.get(0));
-        vl.add(new Vendor(s, 7));
-        assertEquals(vl.get(0).getName(), s);
+    VendorList vendors = new VendorList();
+    ProductList products = new ProductList();
+
+    @BeforeEach
+    void setUp() {
+        vendors.add(new Vendor("Vendor1", 10));
+        vendors.add(new Vendor("CoolVendor2", 10));
+        vendors.add(new Vendor("VendorNumber3", 10));
+        products.add(new Product("abc", "testPoduct"));
+        products.add(new Product("123456789", "testme"));
+        //the first vendor has one product
+        vendors.get(0).addProduct(products.get(0));
+        //the second vendor has both products
+        vendors.get(1).addProduct(products.get(0));
+        vendors.get(1).addProduct(products.get(1));
+        //the third vendor has no products
     }
 
+
+    @Test
+    void getVendorsSellingProduct() {
+        //first product
+        VendorList Result = vendors.getVendorsSellingProduct(products.get(0));
+
+        assertTrue(Result.contains(vendors.get(0)));
+        assertTrue(Result.contains(vendors.get(1)));
+        assertFalse(Result.contains(vendors.get(2)));
+        //second product
+        Result = vendors.getVendorsSellingProduct(products.get(1));
+        assertFalse(Result.contains(vendors.get(0)));
+        assertTrue(Result.contains(vendors.get(1)));
+        assertFalse(Result.contains(vendors.get(2)));
+    }
 }
