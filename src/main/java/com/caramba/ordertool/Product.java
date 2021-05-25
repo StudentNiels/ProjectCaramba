a26eb1448706e82d4082b0dcfccbada9fbf68015
Zanril95 <jeroen-wind@live.nl>
2021-05-25 15:30:46 +0200

    PC-16 Een overzicht krijgen van welke en hoeveel producten er op voorraad zijn

--- src/main/java/com/caramba/ordertool/Product.java
+++ src/main/java/com/caramba/ordertool/Product.java
@@ -4,15 +4,23 @@ public class Product {
     private String productNum;
     private String description;
     private Season season;
+    private int quantity;
+    private int minQuantity;
 
-    public Product(String productNum, String description, Season season) {
+    public Product(String productNum, String description, Season season, int minQuantity) {
         this.productNum = productNum;
         this.description = description;
         this.season = season;
+        this.quantity = 0;
+        this.minQuantity = minQuantity;
     }
 
-    public Product(String productNum, String description) {
-        this(productNum, description, Season.UNDEFINED);
+    public Product(String productNum, String description, int minQuantity) {
+        this.productNum = productNum;
+        this.description = description;
+        this.season = Season.UNDEFINED;
+        this.quantity = 0;
+        this.minQuantity = minQuantity;
     }
 
     //region Getters and Setters
@@ -31,5 +39,41 @@ public class Product {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public Season getSeason() {
+        return season;
+    }
+
+    public void setSeason(Season season) {
+        this.season = season;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getMinQuantity() {
+        return minQuantity;
+    }
+
+    public void setMinQuantity(int minQuantity) {
+        this.minQuantity = minQuantity;
+    }
     //endregion
+
+    public void decrease(){
+        this.quantity--;
+    }
+
+    public void decrease(int amount){
+        this.quantity -= amount;
+    }
+
+    public void increase(int amount){
+        this.quantity =+ amount;
+    }
 }
