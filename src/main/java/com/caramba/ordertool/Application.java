0b38f1db2917c1d2031daf65b49adf27d34d3f56
Bram <23478245+BramB123@users.noreply.github.com>
2021-05-25 16:05:55 +0200

    add loadtest command

--- src/main/java/com/caramba/ordertool/Application.java
+++ src/main/java/com/caramba/ordertool/Application.java
@@ -34,6 +34,7 @@ public class Application {
                 case "link" -> link(command);
                 case "clear" -> clear(command);
                 case "pdf" -> createOrderlistPDF(command);
+                case "loadtest" -> loadTestData();
                 default -> NotificationManager.add(new Notification(NotificationType.ERROR, "Unknown command " + command[0] + ". Use --help to see supported commands"));
             }
         }
@@ -236,6 +237,34 @@ public class Application {
                 pdfc.save();
             }
         }
+    }
+
+    /**
+     * Loads a hardcoded set of test data for preview proposes.
+     */
+    private static void loadTestData(){
+        Product p1 = new Product("12345678", "Wax-Polish", 10);
+        Product p2 = new Product("0010-AA", "Antenneplakkers, zak 100 stuks", 10);
+        Product p3 = new Product("1230", "Insectenschrik", 10);
+        Product p4 = new Product("183247", "Schuim", 10);
+        Product p5 = new Product("2393", "Spray", 10);
+        Product p6 = new Product("3423875", "Voorreiniger", 10);
+        Supplier s1 = new Supplier("Bremen", 7);
+        Supplier s2 = new Supplier("VoorbeeldLeverancier", 14);
+        s1.addProduct(p1);
+        s1.addProduct(p2);
+        s1.addProduct(p3);
+        s2.addProduct(p4);
+        s2.addProduct(p5);
+        s2.addProduct(p6);
+        products.add(p1);
+        products.add(p2);
+        products.add(p3);
+        products.add(p4);
+        products.add(p5);
+        products.add(p6);
+        suppliers.add(s1);
+        suppliers.add(s2);
     }
 
 
