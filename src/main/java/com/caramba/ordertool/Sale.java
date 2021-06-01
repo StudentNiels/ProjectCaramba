a26d2895dead743e18b28928245ee9f20327e567
Bram <23478245+BramB123@users.noreply.github.com>
2021-06-01 19:25:40 +0200

    Fix Sale constructor so it actually uses date

--- src/main/java/com/caramba/ordertool/Sale.java
+++ src/main/java/com/caramba/ordertool/Sale.java
@@ -11,7 +11,7 @@ public class Sale {
     private HashMap<UUID, Integer> products;
 
     public Sale(LocalDate date) {
-        this.date = LocalDate.now();
+        this.date = date;
         this.products = new HashMap<>();
     }
 
