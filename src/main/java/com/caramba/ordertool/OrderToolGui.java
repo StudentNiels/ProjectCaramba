2c40917e455dce98b6d0db2c4b53922c44c092ff
Bram <23478245+BramB123@users.noreply.github.com>
2021-06-07 14:18:46 +0200

    Add the extra test data from the cmd version

--- src/main/java/com/caramba/ordertool/OrderToolGui.java
+++ src/main/java/com/caramba/ordertool/OrderToolGui.java
@@ -71,15 +71,16 @@ public class OrderToolGui extends Application {
     /**
      * Loads a hardcoded set of test data for preview proposes.
      */
-    private static void loadTestData(){
-        Product p1 = new Product("12345678", "Wax-Polish", 10);
-        Product p2 = new Product("0010-AA", "Antenneplakkers, zak 100 stuks", 10);
-        Product p3 = new Product("1230", "Insectenschrik", 10);
-        Product p4 = new Product("183247", "Schuim", 10);
-        Product p5 = new Product("2393", "Spray", 10);
-        Product p6 = new Product("3423875", "Voorreiniger", 10);
+    private void loadTestData(){
+        Product p1 = new Product("12345678", "Wax-Polish");
+        Product p2 = new Product("0010-AA", "Antenneplakkers, zak 100 stuks");
+        Product p3 = new Product("1230", "Insectenschrik");
+        Product p4 = new Product("183247", "Schuim");
+        Product p5 = new Product("2393", "Spray");
+        Product p6 = new Product("3423875", "Voorreiniger");
         Supplier s1 = new Supplier("Bremen", 7);
         Supplier s2 = new Supplier("VoorbeeldLeverancier", 14);
+
         s1.addProduct(p1);
         s1.addProduct(p2);
         s1.addProduct(p3);
@@ -94,7 +95,65 @@ public class OrderToolGui extends Application {
         products.add(p6);
         suppliers.add(s1);
         suppliers.add(s2);
+
+        Sale sl1 = new Sale(LocalDateTime.parse("2020-01-01"));
+        Sale sl2 = new Sale(LocalDateTime.parse("2020-01-12"));
+        Sale sl3 = new Sale(LocalDateTime.parse("2020-02-01"));
+        Sale sl4 = new Sale(LocalDateTime.parse("2020-02-07"));
+        Sale sl5 = new Sale(LocalDateTime.parse("2020-03-01"));
+        Sale sl6 = new Sale(LocalDateTime.parse("2020-04-01"));
+        Sale sl7 = new Sale(LocalDateTime.parse("2020-05-01"));
+        Sale sl8 = new Sale(LocalDateTime.parse("2020-06-01"));
+        Sale sl9 = new Sale(LocalDateTime.parse("2020-07-01"));
+        Sale sl10 = new Sale(LocalDateTime.parse("2020-08-01"));
+        Sale sl11 = new Sale(LocalDateTime.parse("2020-09-01"));
+        Sale sl12 = new Sale(LocalDateTime.parse("2020-10-01"));
+        Sale sl13 = new Sale(LocalDateTime.parse("2020-11-01"));
+        Sale sl14 = new Sale(LocalDateTime.parse("2020-12-01"));
+        Sale sl15 = new Sale(LocalDateTime.parse("2021-01-01"));
+        Sale sl16 = new Sale(LocalDateTime.parse("2021-02-01"));
+        Sale sl17 = new Sale(LocalDateTime.parse("2021-03-01"));
+        Sale sl18 = new Sale(LocalDateTime.parse("2021-04-01"));
+        Sale sl19 = new Sale(LocalDateTime.parse("2021-05-01"));
+        Sale sl20 = new Sale(LocalDateTime.parse("2021-06-01"));
+
+
+        for (Map.Entry<String, Product> entry : products.getProducts().entrySet()) {
+            if(entry.getValue() == p1){
+                //sold in jan 2020
+                sl1.addToProducts(entry.getKey(), 2);
+                sl2.addToProducts(entry.getKey(), 1);
+
+                //feb 2020
+                sl3.addToProducts(entry.getKey(), 1);
+                sl4.addToProducts(entry.getKey(), 1);
+
+                //2020 one per month
+                sl5.addToProducts(entry.getKey(), 4);
+                sl6.addToProducts(entry.getKey(), 8);
+                sl7.addToProducts(entry.getKey(), 7);
+                sl8.addToProducts(entry.getKey(), 10);
+                sl9.addToProducts(entry.getKey(), 20); //July is the peak season!
+                sl10.addToProducts(entry.getKey(), 12);
+                sl11.addToProducts(entry.getKey(), 13);
+                sl12.addToProducts(entry.getKey(), 7);
+                sl13.addToProducts(entry.getKey(), 5);
+                sl14.addToProducts(entry.getKey(), 2);
+
+                //2021
+                sl15.addToProducts(entry.getKey(), 2);
+                sl16.addToProducts(entry.getKey(), 4);
+                sl17.addToProducts(entry.getKey(), 3);
+                sl18.addToProducts(entry.getKey(), 10);
+                sl19.addToProducts(entry.getKey(), 8);
+                sl20.addToProducts(entry.getKey(), 11);
+
+                break;
+            }
+        }
     }
+
+
 
     public enum SceneType{
         PRODUCT_OVERVIEW("/scenes/productOverview.fxml");
