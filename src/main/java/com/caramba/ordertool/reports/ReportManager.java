0bf0fdedb414580e61fa7ce67d86c7ae7aedf845
Bram <23478245+BramB123@users.noreply.github.com>
2021-06-08 10:31:20 +0200

    Add generateProductReport and getReportsByProduct to ReportManager

--- src/main/java/com/caramba/ordertool/reports/ReportManager.java
+++ src/main/java/com/caramba/ordertool/reports/ReportManager.java
@@ -1,5 +1,14 @@
 package com.caramba.ordertool.reports;
 
+import com.caramba.ordertool.MedianYear;
+import com.caramba.ordertool.OrderAlgorithm;
+import com.caramba.ordertool.OrderTool;
+import com.caramba.ordertool.Product;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Year;
+import java.time.YearMonth;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.IntFunction;
@@ -8,9 +17,30 @@ import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
 public class ReportManager {
-    private static List<YearProductReport> yearProductReports;
+    private static List<YearProductReport> yearProductReports = new ArrayList<>();
+    private static OrderAlgorithm orderAlog = new OrderAlgorithm();
 
     public static List<YearProductReport> getYearProductReports() {
         return yearProductReports;
+    }
+
+    public static void generateProductReport(String productId) {
+        MedianYear medianYear = orderAlog.getMedianYear(orderAlog.getDateAmountMap(productId));
+        YearProductReport result = new YearProductReport(Year.now(), medianYear, OrderTool.getProducts().get(productId));
+        for (int i = 1; i <= 6; i++) {
+            int amount = orderAlog.getSoldInYearMonth(productId, YearMonth.of(2021, i));
+            result.add(new MonthProductReport(0, amount, Month.of(i)));
+        }
+        yearProductReports.add(result);
+    }
+
+    public static List<YearProductReport> getReportsByProduct(Product product) {
+        List<YearProductReport> result = new ArrayList<>();
+        for (YearProductReport report : yearProductReports) {
+                if(report.getProduct() == product){
+                    result.add(report);
+                }
+        }
+        return result;
     }
 }
