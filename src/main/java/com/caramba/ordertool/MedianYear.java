21ed2bd2a61dbdddcb7cf4f1d8c00e94c0f8174a
Bram <23478245+BramB123@users.noreply.github.com>
2021-06-08 12:50:27 +0200

    Remove getters for individual months from MedianYear

--- src/main/java/com/caramba/ordertool/MedianYear.java
+++ src/main/java/com/caramba/ordertool/MedianYear.java
@@ -13,110 +13,13 @@ public class MedianYear {
         }
         this.saleQuantities = saleQuantities;
     }
-//#region Per month getters
-    public int getJanuary() {
-        return getByMonthNumber(1);
-    }
 
-    public int getFebruary() {
-        return getByMonthNumber(2);
-    }
-
-    public int getMarch() {
-        return getByMonthNumber(3);
-    }
-
-    public int getApril() {
-        return getByMonthNumber(4);
-    }
-
-    public int getMay() {
-        return getByMonthNumber(5);
-    }
-
-    public int getJune() {
-        return getByMonthNumber(6);
-    }
-
-    public int getJuly() {
-        return getByMonthNumber(7);
-    }
-
-    public int getAugust() {
-        return getByMonthNumber(8);
-    }
-
-    public int getSeptember() {
-        return getByMonthNumber(9);
-    }
-
-    public int getOctober() {
-        return getByMonthNumber(10);
-    }
-
-    public int getNovember() {
-        return getByMonthNumber(11);
-    }
-
-    public int getDecember() {
-        return getByMonthNumber(12);
-    }
-    //#endregion
     public int getByMonthNumber(int monthNumber) {
         if (monthNumber < 1 || monthNumber > 12) {
             throw new IllegalArgumentException();
         }
         return saleQuantities[monthNumber - 1];
     }
-//#region Per month percentage getters
-    public float getJanuaryPercentage() {
-        return getPercentageByMonthNumber(1);
-    }
-
-    public Float getFebruaryPercentage() {
-        return getPercentageByMonthNumber(2);
-    }
-
-    public Float getMarchPercentage() {
-        return getPercentageByMonthNumber(3);
-    }
-
-    public Float getAprilPercentage() {
-        return getPercentageByMonthNumber(4);
-    }
-
-    public Float getMayPercentage() {
-        return getPercentageByMonthNumber(5);
-    }
-
-    public Float getJunePercentage() {
-        return getPercentageByMonthNumber(6);
-    }
-
-    public Float getJulyPercentage() {
-        return getPercentageByMonthNumber(7);
-    }
-
-    public Float getAugustPercentage() {
-        return getPercentageByMonthNumber(8);
-    }
-
-    public Float getSeptemberPercentage() {
-        return getPercentageByMonthNumber(9);
-    }
-
-    public Float getOctoberPercentage() {
-        return getPercentageByMonthNumber(10);
-    }
-
-    public Float getNovemberPercentage() {
-        return getPercentageByMonthNumber(11);
-    }
-
-    public Float getDecemberPercentage() {
-        return getPercentageByMonthNumber(12);
-    }
-//#endregion
     public float getPercentageByMonthNumber(int monthNumber) {
         if (monthNumber < 1 || monthNumber > 12) {
             throw new IllegalArgumentException();
