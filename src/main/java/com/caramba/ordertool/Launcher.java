1cfe2f150514b5f5b1364176c9e4f47812f887b9
Bram <23478245+BramB123@users.noreply.github.com>
2021-06-24 16:30:04 +0200

    Remove unused code for commandline options

--- src/main/java/com/caramba/ordertool/Launcher.java
+++ src/main/java/com/caramba/ordertool/Launcher.java
@@ -3,23 +3,6 @@ package com.caramba.ordertool;
 public class Launcher {
 
     public static void main(String[] args) {
-
-        if (args.length > 1 && args[0].equals("commandline")) {
-            if ("gui".equals(args[0])) {
-                OrderTool.main(removeFirstArg(args));
-            }
-        } else {
-            OrderTool.main(args);
-        }
-    }
-
-    private static String[] removeFirstArg(String[] args) {
-        if (args.length > 1) {
-            String[] newArgs = new String[args.length - 1];
-            System.arraycopy(args, 1, newArgs, 0, args.length - 1);
-            return newArgs;
-        } else {
-            return args;
-        }
+                OrderTool.main(args);
     }
 }
