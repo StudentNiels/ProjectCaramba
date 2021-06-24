bca3b912281e890eb65a0445ddf99e29ee32f21e
Bram <23478245+BramB123@users.noreply.github.com>
2021-06-24 19:18:17 +0200

    Remove unused Product constructor

--- src/main/java/com/caramba/ordertool/models/Product.java
+++ src/main/java/com/caramba/ordertool/models/Product.java
@@ -1,18 +1,14 @@
 package com.caramba.ordertool.models;
 
 public class Product {
-    private String productNum;
-    private String description;
-    private int quantity;
+    private final String productNum;
+    private final String description;
+    private final int quantity;
 
     public Product(String productNum, String description) {
         this.productNum = productNum;
         this.description = description;
         this.quantity = 0;
-    }
-
-    public Product() {
-
     }
 
     //region Getters and Setters
