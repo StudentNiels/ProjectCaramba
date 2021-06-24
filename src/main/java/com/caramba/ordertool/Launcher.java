62cc5b5bac54decdfc9ecad647322c7667b0c261
Bram <23478245+BramB123@users.noreply.github.com>
2021-06-24 19:16:23 +0200

    Add runtime plugin

--- src/main/java/com/caramba/ordertool/Launcher.java
+++ src/main/java/com/caramba/ordertool/Launcher.java
@@ -1,7 +1,7 @@
 package com.caramba.ordertool;
 
 public class Launcher {
-
+    //we require a launcher class because this project isn't modular. (The libraries we use have split packages)
     public static void main(String[] args) {
         OrderTool.main(args);
     }
