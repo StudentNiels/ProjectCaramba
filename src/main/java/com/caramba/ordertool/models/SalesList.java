2c52d94929570566e43d88d9242a0d326fc3ab3b
Bram <23478245+BramB123@users.noreply.github.com>
2021-06-28 16:00:34 +0200

    Remove old comment

--- src/main/java/com/caramba/ordertool/models/SalesList.java
+++ src/main/java/com/caramba/ordertool/models/SalesList.java
@@ -1,13 +1,5 @@
 package com.caramba.ordertool.models;
 
-/*
-Als: medewerker van de inkoop
-
-Wil ik: op basis van het seizoen, verkoop trends, geschatte levertijd en beschikbare voorraad een aanbeveling krijgen van welke producten er bestelt moeten worden.
-
-Zodat ik: tijd kan besparen bij het kiezen van producten om te bestellen.
-*/
-
 import java.time.Year;
 import java.time.YearMonth;
 import java.util.ArrayList;
